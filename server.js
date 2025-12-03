5c909945d6de2847a4d1b7d1459c5bd06745f47c
juan-sabbgh <137013454+sabbaggh@users.noreply.github.com>
2025-12-03 09:20:40 -0600

    test

--- server.js
+++ server.js
@@ -1072,6 +1072,7 @@ app.post('/api/get_recommendation_demo', async (req, res) => {
 app.post('/api/auth/consultant', async (req, res) => {
     try {
         const { name, password } = req.body;
+        console.log(req.body);
         const sqlQuery = `SELECT "Consultant Name" FROM consultants_passwords WHERE "Consultant Name" = '${name}' AND passwords = '${password}';`;
         console.log(`Query to execute for login`);
         const result = await executeQueryAuth(sqlQuery);
